927d4b295cc735128261d50bb8f30e8cae36a53e
Giffen-good <chris6rock@gmail.com>
2021-04-20 00:06:22 -0400

    pp

--- page-in-the-news.php
+++ page-in-the-news.php
@@ -22,7 +22,7 @@ get_header();
  <?php 
   $args = array(
     'post_type' => 'news',
-    'posts_per_page' => 8
+    'posts_per_page' => 20
   );
   $news_query =  new WP_Query($args);
       while ($news_query->have_posts()) :
